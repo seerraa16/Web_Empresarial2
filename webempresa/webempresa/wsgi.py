ae0b55e1130caf293fac40668c17322ae3c4eb68
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-22 12:15:23 +0100

    para verce

--- webempresa/webempresa/wsgi.py
+++ webempresa/webempresa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webempresa.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webempresa.settings")
 
 application = get_wsgi_application()
